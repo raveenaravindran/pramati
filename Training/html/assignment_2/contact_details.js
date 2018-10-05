749a8506c4972adc583df0ef64dc3c81e128d98f
raveenaravindran <raveena.ravindran@imaginea.com>
2018-10-05 08:49:32 +0530

    Assignment 2

--- Training/html/assignment_2/contact_details.js
+++ Training/html/assignment_2/contact_details.js
@@ -1,57 +1,61 @@
+$(document).ready(function() {
 
-$(document).ready(function () {
+	$("#fname").keyup(function () {
+ 		$('#fname').css('textTransform', 'capitalize');
+	});
 
-  $("#fname").keyup(function () {
-    $('#fname').css('textTransform', 'capitalize');
-  });
-  $("#lname").keyup(function () {
+	$("#lname").keyup(function () {
     $('#lname').css('textTransform', 'capitalize');
   });
+	
+	$.validator.addMethod("email", function(value, element)
+		{
+		return this.optional(element) || /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$/i.test(value);
+		}, "Please enter a valid email address.");
+	
+	$.validator.addMethod("phone", function (value, element) {
+		  return this.optional(element) || /^([6-9]+[\d]{9})?$/i.test(value);
+	}, "Please enter valid phone number.");
 
-  $('#contact_form1').validate({
-    rules: {
-      fname: 'required',
-      lname: 'required',
-      date: 'required',
-      phone: 'required',
-      email: 'required',
-    },
-    messages: {
-      fname: 'This field cannot be blank',
-      lname: 'This field cannot be blank',
-      date: 'This field cannot be blank',
-      phone: 'This field cannot be blank',
-      email: 'This field cannot be blank'
-    }
+
+  $("#userForm").validate({
+      rules: {
+          fname: "required",
+					lname: "required",
+          email: {
+              required: true,
+              email: true
+          },
+          phone: {
+              required: true,
+							phone:true,
+              
+							
+          },
+					date: {
+              required: true,
+          },
+          
+      },
+      messages: {
+          fname: "This field is required",
+					lname: "This field is required",
+          email:  {
+              required: "This field is required",
+              email: "Please enter valid email address"
+          },
+          phone: {
+              required: "This field is required",
+							phone:"Please enter valid phone number"
+          },
+          date: {
+              date: "This field is required"
+          },
+      }
   });
 
-  var date_input = $('input[name="date"]');
-  var today = new Date();
-  var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
-  date_input.datepicker({
-    format: 'mm/dd/yyyy',
-    container: container,
-    todayHighlight: true,
-    autoclose: true,
-    endDate: today,
-  });
-
-
-  $("#email").blur(function () {
-    var email = $("#email").val();
-    if (!validateEmail(email)) {
-      $("#email").after('Please enter the valid email');
-    }
-
-  });
-
-  $("#phone").blur(function () {
-    var phone = $(this).val();
-    if (!validatePhone(phone)) {
-      $("#phone").after('Please enter the valid phone number');
-    }
-  });
 });
+
 
 function submitValues() {
   var fname = $("#fname").val();
@@ -60,12 +64,12 @@ function submitValues() {
   var phone = $("#phone").val();
   var dob = $("#date").val();
 
-  if (fname && lname && email && dob) {
-    var table_content = "<tr><td>" + capitalizeLetter(fname) + "</td><td>" + capitalizeLetter(lname) + "</td><td>" + email + "</td><td>" + dob + "</td><td>" + phone + "</td></tr>";
-    $("#contact_info").append(table_content);
-  }
-  document.getElementById("contact_form").reset();
+  var table_content = "<tr><td>" + capitalizeLetter(fname) + "</td><td>" + capitalizeLetter(lname) + "</td><td>" + email + "</td><td>" + dob + "</td><td>" + phone + "</td></tr>";
+  $("#contact_info").append(table_content);
+
+  document.getElementById("userForm").reset();
 }
+
 
 function capitalizeLetter(name) {
   str = name.split(" ");
@@ -75,17 +79,4 @@ function capitalizeLetter(name) {
   str = str.join(" ");
   return str
 }
-
-//Datepicker function
-function validateEmail(email_id) {
-  var pattern = new RegExp(/^([a-zA-Z]+[\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/);
-  return pattern.test(email_id);
-}
-
-//Phone number validation
-function validatePhone(phone) {
-  var ph_pattern = new RegExp(/^([6-9]+[\d]{9})?$/);
-  return ph_pattern.test(phone);
-}
-
 
