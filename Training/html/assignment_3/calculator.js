d9c9f14383a6cad39ef39912cc288bafb7c1973f
raveenaravindran <43139737+raveenaravindran@users.noreply.github.com>
2018-10-09 18:40:11 +0530

    Assignment 3 Changes

--- Training/html/assignment_3/calculator.js
+++ Training/html/assignment_3/calculator.js
@@ -1,105 +1,93 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	$('.btn').click(function(e) {
+  $('.btn').click(function (e) {
+    $('#input').val($('.input').val() + $(this).val());
+  });
 
-		$('#input').val($('.input').val() + $(this).val());
+  $('#all-clear').click(function (e) {
+    $('#input').val('');
+  });
 
-	});
-
-	$('#all-clear').click(function(e) {
-		$('#input').val('');
-	});
-
-	$('#clear').click(function(e) {
-		var el = $("#input");
+  $('#clear').click(function (e) {
+    var el = $("#input");
     var the_value = el.val();
     the_value = the_value.substring(0, the_value.length - 1);
     el.val(the_value);
-	});
-
-	$('.equals').click(function(e) {
-		$('#input').val(eval($('#input').val()));
-	});
-
-	$('.percentage').click(function(e) {
-  	$('#input').val(eval($('#input').val()/100));
   });
 
-  $('.sign').click(function(e) {
-  	$('#input').val(eval($('#input').val()*(-1)));
+  $('.equals').click(function (e) {
+    $('#input').val(eval($('#input').val()));
   });
 
-  $('.btn-pi').click(function(e) {
-  	$('#input').val(eval($('#input').val()*(3.14159)));
+  $('.percentage').click(function (e) {
+    $('#input').val(eval($('#input').val() / 100));
   });
 
-  $('.square-root').click(function(e) {
-  	$('#input').val(SquareRoot($('#input').val()));
+  $('.sign').click(function (e) {
+    $('#input').val(eval($('#input').val() * (-1)));
   });
 
-  $('.factorial').click(function(e) {
-  	$('#input').val(GetFactorial($('#input').val()));
+  $('.btn-pi').click(function (e) {
+    $('#input').val(eval($('#input').val() * (3.14159)));
   });
 
-  $('.exponential').click(function(e) {
-  	$('#input').val(CalcExponential($('#input').val()));
+  $('.square-root').click(function (e) {
+    $('#input').val(SquareRoot($('#input').val()));
   });
 
-  $('.log').click(function(e) {
-  	$('#input').val(CalculateLog($('#input').val()));
+  $('.factorial').click(function (e) {
+    $('#input').val(GetFactorial($('#input').val()));
   });
 
-  $('.sin').click(function(e) {
-  	$('#input').val(CalculateSin($('#input').val()));
+  $('.exponential').click(function (e) {
+    $('#input').val(CalcExponential($('#input').val()));
   });
 
-  $('.cos').click(function(e) {
-  	$('#input').val(CalculateCos($('#input').val()));
+  $('.log').click(function (e) {
+    $('#input').val(CalculateLog($('#input').val()));
   });
 
-  $('.tan').click(function(e) {
-  	$('#input').val(CalculateTan($('#input').val()));
+  $('.sin').click(function (e) {
+    $('#input').val(CalculateSin($('#input').val()));
   });
 
+  $('.cos').click(function (e) {
+    $('#input').val(CalculateCos($('#input').val()));
+  });
 
+  $('.tan').click(function (e) {
+    $('#input').val(CalculateTan($('#input').val()));
+  });
 });
 
-function GetFactorial(value)
- {
-   tempvalue = 1;
-   for ( i=value ; i>0 ;  i-- )
-    {
-       tempvalue *=  i ;
-    }
-return tempvalue
- }
-
-function CalculateLog(value)
- {
-	return Math.log(value)
- }
-
-function CalculateSin(value)
-{
-	return Math.sin(value)
+function GetFactorial(value) {
+  tempvalue = 1;
+  for (i = value; i > 0; i--) {
+    tempvalue *= i;
+  }
+  return tempvalue
 }
 
-function CalculateCos(value)
-{
-	return Math.cos(value)
+function CalculateLog(value) {
+  return Math.log(value)
 }
 
-function CalculateTan(value)
-{
-	return Math.tan(value)
+function CalculateSin(value) {
+  return Math.sin(value)
 }
 
-function CalcExponential(value)
-{
-	return Math.exp(value)
+function CalculateCos(value) {
+  return Math.cos(value)
 }
 
-function SquareRoot(value)
-{
-	return Math.sqrt(value)
+function CalculateTan(value) {
+  return Math.tan(value)
+}
+
+function CalcExponential(value) {
+  return Math.exp(value)
+}
+
+function SquareRoot(value) {
+  return Math.sqrt(value)
 }
