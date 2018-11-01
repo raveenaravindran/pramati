ba4a804120c5316fb788fdce3768fb6c8d86ce5a
raveenaravindran <raveena.ravindran@imaginea.com>
2018-11-01 19:11:39 +0530

    Modification in Swiggy app

--- Training/ruby/assignment_2/swiggy/app/assets/javascripts/application.js
+++ Training/ruby/assignment_2/swiggy/app/assets/javascripts/application.js
@@ -15,4 +15,9 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+= require popper
+//= require bootstrap
+//= require_tree 
+
 
--- Training/ruby/assignment_2/swiggy/app/controllers/users_controller.rb
+++ Training/ruby/assignment_2/swiggy/app/controllers/users_controller.rb
@@ -1,9 +1,28 @@
 class UsersController < ApplicationController
+
+	def index
+	  @user = User.all
+	end
+
 	def new
 		@user = User.new
 	end
 
-	def create
-
+	def show
+	  @user = User.find(params[:id])
 	end
+
+	def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id), alert: "User created successfully."
+    else
+      redirect_to new_user_path, alert: "Error creating user."
+	  end
+	end
+
+	def user_params
+	 params.require(:user).permit(:phone, :name, :email, :password)
+	end
+
 end
